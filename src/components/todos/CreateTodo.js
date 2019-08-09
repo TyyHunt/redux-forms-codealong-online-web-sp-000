f860a46f4014f6214a9a5c457d87f4f64bacc1b3
Tyler Hunt <tyhunt04@gmail.com>
2019-08-09 18:29:39 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -38,10 +38,5 @@ class CreateTodo extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addTodo: formData => dispatch({type: 'ADD_TODO', payload: formData })
-  };
-};
 
-export default connect(null, mapDispatchToProps)(CreateTodo);
+export default connect()(CreateTodo);
